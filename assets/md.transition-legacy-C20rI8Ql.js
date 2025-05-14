b4ac6f146bc16e404384ae1ba4ba62f8ade6e23f
PiFord <cliffordjohnburdeoslutrago@gmail.com>
2025-05-15 03:27:06 +0800

    Updates

--- assets/md.transition-legacy-C20rI8Ql.js
+++ assets/md.transition-legacy-C20rI8Ql.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DT7Y5eMF.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CgwKGGUu.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
